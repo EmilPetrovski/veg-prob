eba496aba504e96df8116a41c86fc873295a328d
Emil Petrovski <emilpetrovski25@gmail.com>
2022-08-29 14:26:15 +0200

    add

---
+++ my-attempt/src/main/java/mk/ukim/finki/wp/exam/example/service/impl/ProductServiceImpl.java
@@ -0,0 +1,66 @@
+package mk.ukim.finki.wp.exam.example.service.impl;
+
+import mk.ukim.finki.wp.exam.example.model.Category;
+import mk.ukim.finki.wp.exam.example.model.Product;
+import mk.ukim.finki.wp.exam.example.model.exceptions.InvalidCategoryIdException;
+import mk.ukim.finki.wp.exam.example.repository.CategoryRepository;
+import mk.ukim.finki.wp.exam.example.repository.ProductRepository;
+import mk.ukim.finki.wp.exam.example.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository, CategoryRepository categoryRepository) {
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<Product> listAllProducts() {
+        return this.productRepository.findAll();
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return this.productRepository.findById(id).orElseThrow(InvalidCategoryIdException::new);
+    }
+
+    @Override
+    public Product create(String name, Double price, Integer quantity, List<Long> categoryIds) {
+        List <Category> categories = this.categoryRepository.findAllById(categoryIds);
+        Product product = new Product(name, price, quantity, categories);
+        return this.productRepository.save(product);
+    }
+
+    @Override
+    public Product update(Long id, String name, Double price, Integer quantity, List<Long> categoryIds) {
+        Product product = this.findById(id);
+        product.setName(name);
+        product.setPrice(price);
+        product.setQuantity(quantity);
+
+        List <Category> categories = this.categoryRepository.findAllById(categoryIds);
+        product.setCategories(categories);
+        return this.productRepository.save(product);
+    }
+
+    @Override
+    public Product delete(Long id) {
+        Product product = this.findById(id);
+        this.productRepository.delete(product);
+        return product;
+    }
+
+    @Override
+    public List<Product> listProductsByNameAndCategory(String name, Long categoryId) {
+        // TODO: implement it
+        return new ArrayList<>();
+    }
+}
